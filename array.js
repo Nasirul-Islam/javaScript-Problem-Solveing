7844417e202cb119525030ede71583e96368976f
Nasirul-Islam <nasirulislam866@gmail.com>
2022-02-07 12:06:58 +0600

    array added

---
+++ array.js
@@ -0,0 +1,11 @@
+const family = ['Fother', 'Mother', 'Sister', 'Brother'];
+// console.log(family);
+// family.push("wife");
+// console.log(family);
+// family.pop();
+// console.log(family);
+// console.log(family.length);
+// console.log(family[2]);
+// console.log(family.indexOf('Mother'));
+// console.log(family.reverse());
+// console.log(family.sort());
