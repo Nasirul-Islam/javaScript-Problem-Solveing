a54453939a8a0479b052c3c051f31debc7d64b7a
Nasirul-Islam <nasirulislam866@gmail.com>
2022-02-07 13:09:07 +0600

    for loop added

---
+++ forLoop.js
@@ -0,0 +1,18 @@
+// যেই লুপ for শব্দ দিয়ে লিখতে হয় এবং for শব্দের পর তিনটা অংশ থাকে।  প্রথম অংশে একটা ভেরিয়েবল ডিক্লেয়ার করা হয়, মাঝখানের অংশে শর্ত থাকে এবং শেষ অংশে ভেরিয়েবল এর মান এক এক করে বাড়ানো হয়। মাঝখানের শর্ত সত্যি হলে for লুপ চলবে আর শর্ত মিথ্যা হলে for লুপ এর কাজ শেষ। 
+
+// The sum of the numbers from 30 to 60
+function sum(num) {
+    let sumation = 0;
+    for (let i = 30; i <= num; i++) {
+        sumation = sumation+i;
+        console.log(i);
+    }
+    console.log(sumation);
+}
+// sum(60);
+
+// Loop over array
+const friends = ['A', 'B', 'C', 'D', 'E']; 
+for (let i = 0; i<friends.length; i++){
+    console.log("Lend me money Mr.", friends[i]);
+}
