f34cc2b8e4e3a313e90852ee29125c0403b0b39f
Nasirul-Islam <nasirulislam866@gmail.com>
2022-02-07 17:24:05 +0600

    yardsToMile add

---
+++ feetToYards.js
@@ -0,0 +1,7 @@
+// Feet to Yards
+function feetToYards(feet) {
+    const yards = feet / 3;
+    return yards;
+}
+const result = feetToYards(10);
+console.log(result, "Feet");
---
+++ yardsToMile.js
@@ -0,0 +1,7 @@
+// Yards To Mile
+function yardsToMile(yards) {
+    const mile = yards / 1760;
+    return mile;
+}
+const result = yardsToMile(2000);
+console.log(result, "Mile");
